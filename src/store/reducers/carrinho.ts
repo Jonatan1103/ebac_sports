04ac11d5280a81afff8311712989af619b9f0531
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2023-06-08 22:17:05 -0300

    finalizado

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type ProdutoState = {
+  itens: Produto[]
+}
+
+const initialState: ProdutoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    adicionarProduto: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (
+        state.itens.find((produtoCarrinho) => produtoCarrinho.id === produto.id)
+      ) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionarProduto } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type FavoritosState = {
+  itensFavorito: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itensFavorito: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produtoFavorito = action.payload
+
+      if (
+        state.itensFavorito.find((produto) => produto.id === produtoFavorito.id)
+      ) {
+        alert('Produto já está na sua lista')
+      } else {
+        state.itensFavorito.push(produtoFavorito)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritosSlice.actions
+export default favoritosSlice.reducer
